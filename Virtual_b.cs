6c5bca10b2a5807932edd459d3515199700b7b4b
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2020-07-01 23:28:35 +0530

    Add files via upload

---
+++ Virtual_b.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Vuforia;
+
+public class vbButton : MonoBehaviour {
+
+    public GameObject vbBtnObj;
+    public Animator cubeAni;
+
+    // Use this for initialization
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("VirtualButton");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+        cubeAni.GetComponent<Animator>();
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        cubeAni.Play("cube_animation");
+        Debug.Log("Button pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        cubeAni.Play("none");
+        Debug.Log("Button released");
+    }
+}
